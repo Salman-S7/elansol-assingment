754f04e95683c0c9eec531d4c6c8dcac1817075a
Salman-S7 <sammyzed1505@gmail.com>
2024-06-30 16:56:06 +0530

    fixed login signup ui

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { Label } from "../components/ui/label";
 import { Input } from "../components/ui/input";
 import { cn } from "../utils/cn";
-import { IconBrandGoogle } from "@tabler/icons-react";
 import { Link } from "react-router-dom";
 import { useSignup } from "../hooks/useSignup";
 
@@ -87,16 +86,6 @@ export default function Signup() {
           </p>
           <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
           <div className="flex flex-col space-y-4">
-            <button
-              className="relative group/btn flex space-x-2 items-center justify-center px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
-              type="submit"
-            >
-              <IconBrandGoogle className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
-              <span className="text-neutral-700 dark:text-neutral-300 text-sm">
-                Google
-              </span>
-              <BottomGradient />
-            </button>
           </div>
         </form>
       </div>

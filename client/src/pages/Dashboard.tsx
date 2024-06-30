470ed54d2d47f5210f27eaafc73f733ae1017ba1
Salman-S7 <sammyzed1505@gmail.com>
2024-06-30 18:23:35 +0530

    completed frontend

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,8 +1,10 @@
-import { useEffect, useState } from "react";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {  useEffect, useState } from "react";
 import { useAuth } from "../hooks/useAuth"
 import { useNavigate } from "react-router-dom";
-import DataTable from "react-data-table-component";
+import DataTable, { TableColumn } from 'react-data-table-component';
 import axios from "axios";
+
 
 const Dashboard = () => {
     const { checkIsAuthenticated } = useAuth();
@@ -20,17 +22,71 @@ const Dashboard = () => {
       navigate('/login');
     }
     getData();
-  }, [usersData])
+  }, [])
   
-  
+
+  const columns: TableColumn<any>[] = [
+    {
+      name: "Image",
+      selector: (row) => row.image,
+      cell: (row) => (
+        <img
+          src={row.image}
+          alt={row.username}
+          style={{
+            width: "40px",
+            height: "40px",
+            borderRadius: "50%",
+            border: "2px solid gray",
+          }}
+        />
+      ),
+      sortable: false,
+    },
+    {
+      name: "Name",
+      selector: (row: { firstName: string }) => row.firstName,
+      sortable: true,
+    },
+    {
+      name: "Email",
+      selector: (row: { email: string }) => row.email,
+      sortable: true,
+    },
+    {
+      name: "Phone",
+      selector: (row: { phone: string }) => row.phone,
+      sortable: true,
+    },
+    {
+      name: "BirthDate",
+      selector: (row: { birthDate: string }) => row.birthDate,
+      sortable: true,
+    },
+    {
+      name: "Role",
+      selector: (row) => row.role,
+      sortable: true,
+    },
+  ];
+
 
   return (
-    <div>
-      <DataTable>
-
-      </DataTable>
+    <div className="h-[100vh]">
+      <DataTable
+        // theme="dark"
+        title="User Data"
+        columns={columns}
+        data={usersData}
+        striped
+        fixedHeader
+        highlightOnHover
+        pagination
+        responsive
+        // customStyles={customStyles}
+      />
     </div>
-  )
+  );
 }
 
 export default Dashboard

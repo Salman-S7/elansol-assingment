f367c11f8b9870d588a943a5eeb0124fe4c91fd5
Salman-S7 <sammyzed1505@gmail.com>
2024-06-30 17:38:15 +0530

    added vercel.json

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,20 +1,35 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useAuth } from "../hooks/useAuth"
 import { useNavigate } from "react-router-dom";
+import DataTable from "react-data-table-component";
+import axios from "axios";
 
 const Dashboard = () => {
     const { checkIsAuthenticated } = useAuth();
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const [usersData, setUsersData] = useState();
 
   useEffect(() => {
+    const getData = async () => {
+      const response = await axios.get(`https://dummyjson.com/users`);
+      console.log(response.data);
+      setUsersData(response.data.users);
+    }
     const authenticated = checkIsAuthenticated();
     if (!authenticated) {
       navigate('/login');
     }
-  },[])
+    getData();
+  }, [usersData])
+  
+  
 
   return (
-    <div>Dashboard</div>
+    <div>
+      <DataTable>
+
+      </DataTable>
+    </div>
   )
 }
 
